ce5bc507ba0c79106f456616ae9b173dfbe740b1
Daniel Becker <daniel.becker000@gmail.com>
2022-04-03 11:22:31 -0400

    commit tree 846339f2af27278426a0fe0a8e39872af4f9e294

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// built and burned 1x 3/24
+// built and burned 2x 4/3 
+
+let names= ["Grandma Judy", "Grandma Zoya", "Barb"];
+
+function writeCards (names, event) {
+    let newArray=[];
+    for (let i=0; i<names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+// obj:invokes console.log once for each number, counting down from the number provided to zero
+// while loop ?
+
+function countDown(number) {
+    while (number>=0){
+        console.log(number--);
+    }
+}
